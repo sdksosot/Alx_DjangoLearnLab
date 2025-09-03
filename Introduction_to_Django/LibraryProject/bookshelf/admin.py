bdac2a9455b87bf6669fb87e7fa39dee6ca18638
sdksosot <aweal5834@gmail.com>
2025-09-04 00:52:01 +0300

    Update admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Book
 
-@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "publication_year")
-    list_filter = ("author", "publication_year")
+    list_display = ("title", "author", "publication_year")  # الأعمدة اللي تظهر
+    list_filter = ("publication_year", "author")            # علشان تعمل فلترة
+    search_fields = ("title", "author")                     # هنا بتعمل بحث بالعنوان أو الكاتب
+
+admin.site.register(Book, BookAdmin)
