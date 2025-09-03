6f6a28870e4d93e1a641a67cf834c9d76100a527
sdksosot <aweal5834@gmail.com>
2025-09-04 00:49:51 +0300

    Update admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,3 +4,4 @@ from .models import Book
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
+    list_filter = ("author", "publication_year")
