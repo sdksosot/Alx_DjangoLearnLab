61b668bc03b3802fe10390205baccec97c990b2d
sdksosot <aweal5834@gmail.com>
2025-09-17 02:58:19 +0300

    Update models.py

--- django_models/relationship_app/models.py
+++ django_models/relationship_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class Author(models.Model):
+class Author (models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -29,3 +29,4 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
