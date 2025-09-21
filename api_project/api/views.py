833122c8cd39eab00a9fa9b61002f9b6b31cf0f4
sdksosot <aweal5834@gmail.com>
2025-09-21 22:04:52 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,9 +8,12 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 
 # الجديد (CRUD كامل باستخدام ViewSet)
+
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # ← لازم يكون مسجل دخول
+
 
 
 
