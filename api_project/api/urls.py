8049394257574436113ee79b756fb78de5d6b372
sdksosot <aweal5834@gmail.com>
2025-09-21 21:48:29 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from .views import BookList   # هنا بنتأكد إننا مستوردين BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+# إنشاء الراوتر
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Endpoint القديم (لستة الكتب فقط)
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Endpoints الخاصة بالـ CRUD (BooksViewSet)
+    path('', include(router.urls)),
 ]
