07a544d8dad8d616d278bd1e5c6c97c49bc0a5c8
sdksosot <aweal5834@gmail.com>
2025-09-21 03:56:52 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # هنا ربطنا ال api
+    path('api/', include("api.urls")),  # هنا ربطنا ال api
 ]
