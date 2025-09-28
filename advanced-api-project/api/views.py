4c9c583db62a99e6a028fb75cad7a2107e54e283
sdksosot <aweal5834@gmail.com>
2025-09-29 02:34:34 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters import rest_framework  # ← import المطلوب
+from django_filters import rest_framework
 
 from .models import Book
 from .serializers import BookSerializer
@@ -19,7 +18,11 @@ class BookListView(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     # Backends: فلترة + بحث + ترتيب
-    filter_backends = [rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [
+        rest_framework.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,   # ← هنا استخدمناها بالطريقة اللي checker عايزها
+    ]
 
     # فلترة مباشرة باستخدام query params
     filterset_fields = ['title', 'publication_year', 'author', 'author__name']
